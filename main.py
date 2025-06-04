acb4f26619b7988dd3d14473afcec656e5c9280c
harishvow <harisharish982005@gmail.com>
2025-06-04 21:53:59 +0530

    finished this webapp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import plotly.express as px
-
+from collections import Counter
 from apicall import get_data
 
 st.title("Weather Forecast For Next Days")
@@ -22,13 +22,16 @@ if place:
             st.plotly_chart(figure)
 
         elif option == "Sky":
-            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
-            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
-            image_paths = [images[condition] for condition in sky_conditions]
-            print(sky_conditions)
-            st.image(image_paths, width=115)
-    except KeyError:
-        st.write("Invaild Place ")
+            images = {
+                "Clear": "images/clear.png",
+                "Clouds": "images/cloud.png",
+                "Rain": "images/rain.png",
+                "Snow": "images/snow.png"
+            }
+            sky_conditions = [entry["weather"][0]["main"] for entry in filtered_data]
+            most_common_condition = Counter(sky_conditions).most_common(1)[0][0]
+            st.image(images[most_common_condition], caption=most_common_condition, width=150)
 
+    except KeyError:
+        st.error("Invalid place. Please enter a valid city name.")
 
